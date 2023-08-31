d63232ace8d4401422724ad7f3cec290d7b4d430
Angel Velasquez <avelasquezn@gmail.com>
2023-08-31 08:33:05 -0500

    feat(register-developer): added developerRegistered event emit behavior.

--- src/app/welcoming/components/register-developer/register-developer.component.ts
+++ src/app/welcoming/components/register-developer/register-developer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
@@ -12,7 +12,15 @@ export class RegisterDeveloperComponent {
     lastName: new FormControl(''),
   });
 
+  @Output() developerRegistered = new EventEmitter<{ firstName: string, lastName: string }>;
   onSubmit() {
     console.log(this.developerForm.value);
+    let firstName = this.developerForm.value.firstName != null ? this.developerForm.value.firstName.toString() : '';
+    let lastName = this.developerForm.value.lastName != null ? this.developerForm.value.lastName.toString() : '';
+    this.onDeveloperRegistered(firstName, lastName);
+  }
+
+  onDeveloperRegistered(firstName: string, lastName: string ) {
+    this.developerRegistered.emit({firstName, lastName});
   }
 }
