99db36334420d1eddb1be6e1873704716a794c25
Angel Velasquez <avelasquezn@gmail.com>
2023-08-31 07:53:59 -0500

    feat(welcome): added welcome component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RegisterDeveloperComponent } from './welcoming/components/register-developer/register-developer.component';
+import { WelcomeComponent } from './welcoming/components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterDeveloperComponent
+    RegisterDeveloperComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
