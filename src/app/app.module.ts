ecab2f66542f53e8cf53ab3ec0e935f504b16d76
Angel Velasquez <avelasquezn@gmail.com>
2023-08-31 07:51:34 -0500

    feat(register-developer): added register developer component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterDeveloperComponent } from './welcoming/components/register-developer/register-developer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterDeveloperComponent
   ],
   imports: [
     BrowserModule
